49d51f7e5b4be57381055782fa7a4dccd6271237
pankajj42 <pankajjangid42@gmail.com>
2024-12-04 20:17:18 +0530

    Reapply "Completed Week-2 Async JS Assignments"
    This reverts commit 03d745b97aab9e91c9c57f2b6504f90f13aa0939.

--- week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
+++ week-2/01-async-js/hard (promises)/1-promisify-setTimeout.js
@@ -3,6 +3,10 @@
 */
 
 function wait(n) {
+	return new Promise((resolve, reject) => {
+		if (typeof n !== "number" || n < 0) reject("Invalid Input Time");
+		else setTimeout(resolve, n * 1000);
+	});
 }
 
 module.exports = wait;
