76a0bad2e0b48820779cd54b1e4b6db48628cdc0
pankajj42 <pankajjangid42@gmail.com>
2025-01-01 15:26:21 +0530

    useMemo Assignments done

--- week-6/1-use-memo/src/components/Assignment3.jsx
+++ week-6/1-use-memo/src/components/Assignment3.jsx
@@ -1,27 +1,45 @@
-import React, { useState, useMemo } from 'react';
+import React, { useState, useMemo } from "react";
 // You have been given a list of items you shopped from the grocery store
 // You need to calculate the total amount of money you spent
 
 export const Assignment3 = () => {
-    const [items, setItems] = useState([
-        { name: 'Chocolates', value: 10 },
-        { name: 'Chips', value: 20 },
-        { name: 'Onion', value: 30 },
-        { name: 'Tomato', value: 30 },
-        // Add more items as needed
-    ]);
+	const [items, setItems] = useState([
+		{ name: "Chocolates", value: 10 },
+		{ name: "Chips", value: 20 },
+		{ name: "Onion", value: 30 },
+		{ name: "Tomato", value: 30 },
+		// Add more items as needed
+	]);
 
-    // Your code starts here
-    const totalValue = 0;
-    // Your code ends here
-    return (
-        <div>
-            <ul>
-                {items.map((item, index) => (
-                    <li key={index}>{item.name} - Price: ${item.value}</li>
-                ))}
-            </ul>
-            <p>Total Value: {totalValue}</p>
-        </div>
-    );
+	// Your code starts here
+	const totalValue = useMemo(() => {
+		console.log("Recalculating Total Value");
+		return items.reduce(({ value: v1 }, { value: v2 }) => {
+			return { value: v1 + v2 };
+		}).value;
+	}, [items]);
+	// Your code ends here
+	return (
+		<div>
+			<ul>
+				{items.map((item, index) => (
+					<li key={index}>
+						{item.name} - Price: $
+						<input
+							id={index}
+							type="number"
+							value={item.value}
+							onChange={(e) => {
+								items[e.target.id].value = parseInt(
+									e.target.value
+								);
+								setItems([...items]);
+							}}
+						></input>
+					</li>
+				))}
+			</ul>
+			<p>Total Value: {totalValue}</p>
+		</div>
+	);
 };
